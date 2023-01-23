e81a97da45211845482eff25b1728511ab82b357
Chotiya Priya <49747628+Priya-Chotiya@users.noreply.github.com>
2023-01-23 16:52:05 +0530

    Small Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React ,{useEffect}from 'react'
 import CTA from './CTA'
 import './header.css'
 import ME from '../../assets/me-2.png'
 import HeaderSocials from './HeaderSocials'
+
+import CryptoJS from 'crypto-js'
+
 const Header = () => {
+  // useEffect(() =>{
+  //   let name = CryptoJS.AES.encrypt("priya" , "sass").toString();
+  //   document.cookie =   "userName=" + name + ";path=" + window.location.href; 
+  //   document.cookie =   "passWord=kkkk;path=" + window.location.href; 
+  // } , [])
+
+  
+  // setTimeout(() => {
+  //   let username = getCookie('userName');
+  //   console.log(CryptoJS.AES.decrypt(username, "sass").toString(CryptoJS.enc.Utf8) , 'username')
+
+  //   let password = getCookie('passWord');
+  //   console.log(password  , "password")
+  // } , 5000)
+
+  // function getCookie(cookieName){
+  //   let name  = cookieName + "=";
+  //   let decodedName  = decodeURIComponent(document.cookie).split(';');
+   
+  //     for (let index = 0; index < decodedName.length; index++) {
+  //       let element = decodedName[index];
+  //       while(element.charAt(0) == ' '){
+  //         element = element.substring(1)
+  //       }
+  //       if(element.indexOf(name) == 0){
+  //           return element.substring(name.length , element.length)
+  //       }
+        
+  //     }
+
+  //     return "";
+  // }
   return (
     <header>
       <div className="container header_container">
